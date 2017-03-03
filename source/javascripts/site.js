fd88554946dfd84d3185f66eabeac9b7d05c8279
Silas Sao <silassao@gmail.com>
2017-03-02 16:06:58 -0800

    finish markup for desktop

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,25 @@
 jQuery(document).ready(function($) {
-	$('.hero-slider').unslider({
-    arrows: false
+  $('.hero-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    autoplay: true,
+  });
+
+  $('.hero-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+     //remove all active class
+     $('.hero-slider-nav .slick-slide').removeClass('slick-current');
+     //set active class for current slide
+     $('.hero-slider-nav .slick-slide').eq(currentSlide).addClass('slick-current');
+  });
+
+  $('.hero-slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.hero-slider',
+    focusOnSelect: true,
+    vertical: true,
+    autoplay: true
   });
 });
