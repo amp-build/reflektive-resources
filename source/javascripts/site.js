7e00a8db17d47b8d9505038538a71ecea69c4b6b
Silas Sao <silassao@gmail.com>
2017-03-07 17:30:13 -0800

    finish mobile

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,27 @@
+//= require bootstrap-sprockets
+
 jQuery(document).ready(function($) {
+  $('.resource-type-filter-dropdown a').on('click', function(e){
+    e.preventDefault();
+    $('.resource-type-filter').toggleClass('visible-lg');
+  });
+  
   $('.hero-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     vertical: true,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: true,
+          vertical: false,
+          swipeToSlide: true
+        }
+      }
+    ]
   });
 
   $('.hero-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
